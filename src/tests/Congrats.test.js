9a45fe6e6b6d83b64059ce9f64538df2fa5898b5
Bohdan Drobinskyi <nimrik@MacBook-Pro-Bohdan.local>
2020-04-19 13:05:38 +0300

    Add proptypes for Congrats components

--- src/tests/Congrats.test.js
+++ src/tests/Congrats.test.js
@@ -2,15 +2,27 @@ import React from "react";
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
-import { findByTestAttr } from "./testUtils";
+import { findByTestAttr, checkProps } from "./testUtils";
 import Congrats from "../components/Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-const setup = (props = {}) => shallow(<Congrats {...props} />);
+const defaultProps = { success: false };
+
+/*
+* Factory function to create a ShallowWrapper for the GuessedWords component
+* @function setup
+* @param {object} props - Component props specific to this setup
+* @returns {ShallowWrapper}
+* */
+
+const setup = (props = {}) => {
+    const setupProps = { ...defaultProps, ...props }
+    return shallow(<Congrats {...setupProps} />)
+};
 
 test('render without error', () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, 'component-congrats');
   expect(component.length).toBe(1);
 });
@@ -25,4 +37,9 @@ test('renders non-empty congrats message when `success` props is true', () => {
     const wrapper = setup({success: true});
     const message = findByTestAttr(wrapper, 'congrats-message');
     expect(message.text().length).not.toBe(0);
+});
+
+test('does not throw warning with expected props', () => {
+   const expectedProps = { success: false };
+   checkProps(Congrats, expectedProps)
 });
